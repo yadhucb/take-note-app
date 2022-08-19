1de793d8724138c9abfa5a3dd58bdccef495a8a5
yadhu <yadhucb96@gmail.com>
2022-08-19 22:52:01 +0530

    note date and time added

--- TakeNote/settings.py
+++ TakeNote/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Kolkata'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
