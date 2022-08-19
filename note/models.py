5df92557224511eba6cb2098306466ac70c9922e
yadhu <yadhucb96@gmail.com>
2022-08-19 17:09:32 +0530

    note model created

--- note/models.py
+++ note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=150, null=True, blank=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'note_images', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[:10]
+
